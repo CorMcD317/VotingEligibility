6b9bec517b95c7e441318ddbef403d6e68baafe0
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-10-23 07:37:01 -0400

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+age = int(input("Your age here: "))
+
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+# I don't understand the bool notes very well. So I choose to use if statements
